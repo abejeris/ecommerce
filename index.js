5b62f0ab3e60ac8503251268889aa0ea9ae44cb5
Audrius Bejeris <audriusbejeris@gmail.com>
2024-05-22 21:37:13 +0300

    added express functionality

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-console.log('hi');
+const express = require('express');
+
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+
+app.get('/', (req, res) => {
+	res.send(`
+        <div>
+            <form method="POST">
+                <input name="email" placeholder="email"/>
+                <input name="password" placeholder="password"/>
+                <input name="passwordConfirmation" placeholder="password confirmation"/>
+                <button>Sign Up</button>
+            </form>
+        </div>
+    `);
+});
+
+app.post('/', (req, res) => {
+	const formData = {
+		email: req.body.email,
+		password: req.body.password,
+		passwordConfirmation: req.body.passwordConfirmation,
+	};
+	console.log(formData);
+
+	res.send('Account created');
+});
+
+app.listen(3000, () => {
+	console.log('app is running on port 3000');
+});
